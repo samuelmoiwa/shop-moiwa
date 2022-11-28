a03a0b95ad395d7d0ecc481b6d3e79f84512b481
Moiwa <38554511+samuelmoiwa@users.noreply.github.com>
2022-11-28 10:51:24 +0000

    add productsAction.js

---
+++ src/redux/actions/productsActions.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/named */
+import { ActionTypes } from '../constants/action-types';
+
+export const setProducts = (products) => ({
+  type: ActionTypes.SET_PRODUCTS,
+  payload: products,
+});
+
+export const selectedProduct = (product) => ({
+  type: ActionTypes.SELECTED_PRODUCT,
+  payload: product,
+});
+export const removeSelectedProduct = () => ({
+  type: ActionTypes.REMOVE_SELECTED_PRODUCT,
+});
